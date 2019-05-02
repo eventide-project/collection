c7ecb8422311aa855a55c583d485389a4c1091f0
Scott Bellware <sbellware@gmail.com>
2019-05-01 21:44:31 -0400

    Refinement is removed Set factory method replaces the refinement

--- lib/collection/collection.rb
+++ lib/collection/collection.rb
@@ -2,4 +2,20 @@ module Collection
   def self.[](type_parameter)
     Set[type_parameter]
   end
+
+  def self.Set(items)
+    items = Array(items)
+
+    raise ArgumentError, "Collection can't be empty" if items.empty?
+
+    type_parameter = items[0].class
+
+    set = Set[type_parameter].new
+
+    items.each do |member|
+      set.add member
+    end
+
+    set
+  end
 end
