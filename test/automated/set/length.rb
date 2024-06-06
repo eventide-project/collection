ed9ab57148f04abdf0e42bcb0e32c9c5fe53a8ef
Orban Botond <orbanbotond@gmail.com>
2024-06-06 11:55:34 +0300

    Add more descriptive variables to the test

--- test/automated/set/length.rb
+++ test/automated/set/length.rb
@@ -5,7 +5,13 @@ context "Set" do
     collection = Collection::Set.new(String)
 
     collection.add('something')
+    initial_length = collection.length
     collection.add('something')
+    length_difference = initial_length - collection.length
+    length_has_no_difference = length_difference == 0
+
+    test { assert(length_has_no_difference) }
+
     collection.add('something else')
 
     length = collection.length
